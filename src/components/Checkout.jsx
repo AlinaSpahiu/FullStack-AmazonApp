8dc68aa12294b606431bed04050d766bba5d1ed8
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-09-09 19:47:34 +0200

    Create Checkout.jsx

---
+++ src/components/Checkout.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './styles/Checkout.css';
+import Subtotal from "./Subtotal";
+
+
+function Checkout() {
+    return (
+      
+        <div className="checkout">
+            <div className="checkout-left">
+                {/* Banner - Ad */}
+                <img className="checkout-ad" 
+                     src="https://www.tea-for-two.com/wp-content/uploads/2013/10/teafortwo_web_banners_rumbo_2_1.gif" />
+            
+                <div>
+                   <h2 className="checkout-title">Your shopping basket</h2>
+                </div>
+            </div>
+
+           <div className="checkout-right">
+               <Subtotal />
+               
+            </div>
+        </div>
+    )
+}
+
+export default Checkout
