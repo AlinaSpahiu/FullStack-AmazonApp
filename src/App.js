8dabdf17f73a24f7bbeefe19d856e5f3f81a9446
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-09-09 19:47:16 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Header from "./components/Header"; 
+import Home from "./components/Home";
+import Checkout from "./components/Checkout"
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+       <div className="App"> 
+          <Switch>
+
+              <Route path="/checkout">
+                 <Header />
+                 <Checkout />
+              </Route>
+
+              <Route path="/">
+                 <Header />
+                  <Home />
+              </Route>
+              
+          </Switch>
+       </div>
+    </Router>
+  );
+}
+
+export default App;
