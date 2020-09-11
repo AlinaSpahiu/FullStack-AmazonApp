cffaa0abe781db9b2eb354de5488ad3a02e723ca
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-09-11 13:34:31 +0200

    Update Product.jsx

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 import "./styles/Product.css"
 import StarIcon from '@material-ui/icons/Star';
+import {useStateValue} from "../StateProvider";
 
-function Product({title, image, price, rating}) {
+function Product({id, title, image, price, rating}) {
+    
+    const [{basket}, dispatch] = useStateValue();
+    
+    const addToBasket = () => {
+        //dispatch the item into the data layer:
+        dispatch({
+            type: 'ADD_TO_BASKET',
+            item:{
+                id: id,
+                title: title,
+                image: image,
+                price: price,
+                rating: rating,
+            },
+        })
+    }
     return (
         <div className="product">
             <div className="product-info">
@@ -25,7 +42,7 @@ function Product({title, image, price, rating}) {
             <img src={image}
                  alt="" />
 
-            <button> Add to basket</button>
+            <button onClick={addToBasket}> Add to basket</button>
         </div>
     )
 }
