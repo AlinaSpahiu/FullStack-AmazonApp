030ab1afa72d9a64043d4c68b0e0b6f323719fbd
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-09-11 13:34:06 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Header from "./components/Header"; 
 import Home from "./components/Home";
 import Checkout from "./components/Checkout"
-import './App.css';
+import Login from "./components/Login";
+import {auth} from "./firebase";
+import {useStateValue} from "./StateProvider";
 
 function App() {
+   const [{}, dispatch] = useStateValue();
+
+   useEffect( () =>{
+
+  // will only run once when the app component loads
+  auth.onAuthStateChanged(authUser =>{
+     console.log("User is >>>", authUser);
+
+     if(authUser){
+        //the user just loggedin / the user was logged in
+        dispatch({
+           type: "SET_USER",
+           user: authUser
+        })
+     } else {
+        // the user is  logged out
+        dispatch({
+           type:'SET_USER',
+           user: null // if user logges out, than set user to null
+        })
+     }
+  }   )
+   }, [])
+
   return (
     <Router>
        <div className="App"> 
+         
           <Switch>
+
+              <Route path="/login">
+                 <Login />
+              </Route>
 
               <Route path="/checkout">
                  <Header />
@@ -17,10 +48,10 @@ function App() {
               </Route>
 
               <Route path="/">
-                 <Header />
+              <Header />
                   <Home />
               </Route>
-              
+
           </Switch>
        </div>
     </Router>
