1c2d9446478f4c071b703a473c0944d15f45e643
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-09-16 23:05:15 +0200

    Update Checkout.jsx

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -9,7 +9,6 @@ import {useStateValue} from "../StateProvider";
 function Checkout() {
     const [{basket, user}, dispatch] = useStateValue();
     return (
-      
         <div className="checkout">
             <div className="checkout-left">
                 {/* Banner - Ad */}
