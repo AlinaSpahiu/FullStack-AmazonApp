198f70a711b1d56039580cb1fa2e66ee05a78355
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-09-16 23:04:58 +0200

    Update Checkout.jsx

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -29,9 +29,7 @@ function Checkout() {
                              rating={item.rating}
                          />
                    ))}
-                   
-
-                       
+                      
                 </div>
             </div>
 
