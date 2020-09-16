ab9adbee8fdf34f9d7e0d5dc36c97fa5edab8121
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-09-16 23:07:12 +0200

    Update Checkout.jsx

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -7,7 +7,9 @@ import { getBasketTotal } from '../reducer';
 import {useStateValue} from "../StateProvider";
 
 function Checkout() {
+    
     const [{basket, user}, dispatch] = useStateValue();
+    
     return (
         <div className="checkout">
             <div className="checkout-left">
